1eea02e328c01cf828155d34ee91212f66e0eca0
Felix Thea <felixthea@gmail.com>
2013-04-07 18:27:25 -0400

    associate fight and round models and create routes

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,3 +1,4 @@
 class Fight < ActiveRecord::Base
   attr_accessible :date, :fighter_a, :fighter_b
+  has_many :rounds
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,4 @@
 class Round < ActiveRecord::Base
   attr_accessible :fighter_a_score, :fighter_b_score
+  belongs_to :fight
 end
