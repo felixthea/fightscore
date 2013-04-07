151384446c1a7e666179bf48bb88cf989483a94b
Felix Thea <felixthea@gmail.com>
2013-04-07 18:01:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fightscore::Application.config.secret_token = '3e814955a2fd9817778b8ae0706743c9d1963df2c935abd4a76e6adb37ed2b9f0bf492b927c299f3d4c47ba2e3bdf45cb6723acf049b3dea5090b7916981be08'
