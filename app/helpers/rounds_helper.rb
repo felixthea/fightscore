4e434f44a84c4d6960173ccc8b5ab35520396f32
Felix Thea <felixthea@gmail.com>
2013-04-21 15:06:02 -0400

    trivial change

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -1,2 +1,10 @@
 module RoundsHelper
+  ##
+  #def create_rounds(num_of_rounds, fightid, fighterid)
+  #  i = 1
+  #  num_of_rounds.times do
+  #    Round.create(fight_id: fightid, fighter_id: fighterid, roundname: i)
+  #    i++
+  #  end
+  #end
 end
